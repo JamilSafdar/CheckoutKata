9db519c0e3f7575c10c4999769a42700cfe9dd8c
Jamil Safdar <jamil.safdar@ao.com>
2022-02-15 20:43:31 +0000

    first test added

--- ShoppingCartKata/UnitTests/CheckoutTests.cs
+++ ShoppingCartKata/UnitTests/CheckoutTests.cs
@@ -1,18 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using NUnit.Framework;
 
 namespace ShoppingCartKata
 {
     public class Tests
     {
+        private Checkout _checkout;
+
         [SetUp]
         public void Setup()
         {
+            _checkout = new Checkout();
         }
 
         [Test]
-        public void Test1()
+        public void GivenOneItem_ReturnPrice()
         {
-            Assert.Pass();
+            var item = "A99";
+            var expectedPrice = 0.50m;
+            var output = _checkout.Scan(item);
+
+            Assert.AreEqual(expectedPrice, output);
         }
+    }
+
+    public class Checkout
+    {
+        List<string> itemList = new List<string>();
+        public decimal Scan(string item)
+        {
+           itemList.Add(item);
+           var sum = itemList.Count;
+           return sum;
+        }
+        
     }
 }
