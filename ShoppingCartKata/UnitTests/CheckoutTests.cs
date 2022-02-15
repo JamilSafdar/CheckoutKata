ccb8b10f358fa5a2c451cffb345b3eedaf8e23f6
Jamil Safdar <jamil.safdar@ao.com>
2022-02-15 21:06:59 +0000

    discount test added

--- ShoppingCartKata/UnitTests/CheckoutTests.cs
+++ ShoppingCartKata/UnitTests/CheckoutTests.cs
@@ -35,17 +35,34 @@ namespace ShoppingCartKata
             Assert.AreEqual(expectedPrice, output);
 
         }
+        [Test]
+        public void GivenThreeIdenticalItems_ReturnDiscountedPrice()
+        {
+            var item = "A99";
+            var expectedPrice = 1.30M;
+            _checkout.Scan(item);
+            _checkout.Scan(item);
+            var output = _checkout.Scan(item);
+            Assert.AreEqual(expectedPrice, output);
+
+        }
     }
 
     public class Checkout
     {
         List<string> itemList = new List<string>();
+
         public decimal Scan(string item)
         {
-           itemList.Add(item);
-           var sum = itemList.Count * 0.5;
-           return (decimal)sum;
+            itemList.Add(item);
+            var discount = 0M;
+            var sum = itemList.Count * 0.5;
+            if (itemList.Count == 3)
+            {
+                discount = (decimal )sum * 0.13M;
+
+            }
+            return (decimal) sum - discount;
         }
-        
     }
 }
